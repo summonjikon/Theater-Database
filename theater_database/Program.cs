88b903cada28c9788e003ee244791094700907be
jikon <jikon@protonmail.com>
2022-02-07 14:39:40 +0100

    Update

--- theater_database/Program.cs
+++ theater_database/Program.cs
@@ -6,18 +6,78 @@ namespace theater_database
     {
         static void Main(string[] args)
         {
-            Theater theTheater = new Theater();
+            int genre;
+            string filmKeuze;
 
-            Movie firstMovie = new Movie("Pulp Fiction");
-            Movie secondMovie = new Movie("Snatch");
+            Theater actieFilm = new Theater();
+            Theater horrorFilm = new Theater();
+            Theater scifiFilm = new Theater();
 
-            theTheater.AddMovie(firstMovie);
-            theTheater.AddMovie(secondMovie);
 
-            foreach(Movie a in theTheater.GetTitles())
+            Movie A1 = new Movie("Pulp Fiction");
+            Movie A2 = new Movie("Snatch");
+
+            Movie H1 = new Movie("The Grudge");
+            Movie H2 = new Movie("The Ring");
+
+            Movie C1 = new Movie("2001: A Space Odyssey");
+            Movie C2 = new Movie("Interstellar");
+
+            horrorFilm.AddMovie(H1);
+            horrorFilm.AddMovie(H2);
+
+            actieFilm.AddMovie(A1);
+            actieFilm.AddMovie(A2);
+
+            scifiFilm.AddMovie(C1);
+            scifiFilm.AddMovie(C2);
+
+
+
+
+            Console.WriteLine("Welkom,\nKies uit een film categorie door een cijfer in te vullen\n \n1. Actie \n2. Horror \n3. Sci-Fi ");
+            genre = Convert.ToInt32(Console.ReadLine());
+
+
+            if(genre == 1)
             {
-                Console.WriteLine(a.GetTitle());
+                Console.Clear();
+                foreach (Movie a in actieFilm.GetTitles())
+                {
+                    Console.WriteLine(a.GetTitle());
+                }
             }
+            else if (genre == 2)
+            {
+                Console.Clear();
+                foreach (Movie a in horrorFilm.GetTitles())
+                {
+                    Console.WriteLine(a.GetTitle());
+                }
+            }
+            else if (genre == 3)
+            {
+                Console.Clear();
+                foreach (Movie a in scifiFilm.GetTitles())
+                {
+                    Console.WriteLine(a.GetTitle());
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nGeen geldig antwoord");
+            }
+
+
+            Console.WriteLine("\nSelecteer een film door een title in te typen");
+            filmKeuze = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine(filmKeuze + " Is helaas uitverkocht!");
+
+            
+
+
         }
     }
 }
